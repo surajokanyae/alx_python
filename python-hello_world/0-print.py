bbb42c9189f7d809b1e6b7ef8c5477a54f2a060b
sgkanya <sgkanyae@gmail.com>
2023-07-19 18:20:48 +0100

    HelloWorld

--- python-hello_world/0-print.py
+++ python-hello_world/0-print.py
@@ -1,5 +1,1 @@
-def print_puzzle():
-    print("Programming is like building a multilingual puzzle.")
-
-if __name__ == "__main__":
-    print_puzzle()
+print("Programming is like building a multilingual puzzle.")
