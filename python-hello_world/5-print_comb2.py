18c80bae41be7c87d8b3847e068b3b95289d4fea
sgkanya <sgkanyae@gmail.com>
2023-07-20 21:25:00 +0100

    update

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,8 +1,9 @@
-def print_comb2():
-  """Prints all two-digit combinations separated by commas."""
-  for i in range(10):
-    for j in range(10):
-      print(str(i) + "," + str(j), end="")
+def print_numbers(start, end):
+  """Prints numbers from start to end, separated by ,, followed by a space."""
+  for i in range(start, end + 1):
+    print("{0:02d}, ".format(i), end="")
+
+  print()
 
 if __name__ == "__main__":
-  print_comb2()
+  print_numbers(0, 100)
