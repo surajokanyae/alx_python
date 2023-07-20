b41400e82548185d3c7a8a6f2920ee8c36785d4b
sgkanya <sgkanyae@gmail.com>
2023-07-20 21:20:00 +0100

    HelloWorld

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,8 +1,8 @@
-def comb2():
+def print_comb2():
   """Prints all two-digit combinations separated by commas."""
   for i in range(10):
     for j in range(10):
       print(str(i) + "," + str(j), end="")
 
 if __name__ == "__main__":
-    comb2()
+  print_comb2()
