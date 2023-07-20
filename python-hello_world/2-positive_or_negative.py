092db124ffff32607c42afb1de57927b7a8b418b
sgkanya <sgkanyae@gmail.com>
2023-07-20 15:17:08 +0100

    HelloWorld

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -1,6 +1,6 @@
 import random
 
-def positive_or_negative(number):
+def positive_or_negative():
   number = random.randint(-100, 100)
   if number > 0:
     print(f"{number} is positive")
@@ -10,12 +10,4 @@ def positive_or_negative(number):
     print(f"{number} is negative")
 
 if __name__ == "__main__":
-  positive_or_negative(-4)
-  positive_or_negative(0)
-  positive_or_negative(-3)
-  positive_or_negative(-10)
-  positive_or_negative(10)
-  positive_or_negative(-5)
-  positive_or_negative(6)
-  positive_or_negative(7)
-  positive_or_negative(5)
+  positive_or_negative()
