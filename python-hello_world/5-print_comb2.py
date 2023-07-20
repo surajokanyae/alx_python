9c590384706565ddfdfe2ec86615be76fc6dd035
sgkanya <sgkanyae@gmail.com>
2023-07-20 21:32:52 +0100

    HelloWorld

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,9 +1,10 @@
 def print_numbers(start, end):
   """Prints numbers from start to end, separated by ,, followed by a space."""
   for i in range(start, end + 1):
-    print("{0:02d}, ".format(i), end="")
+    print(f"{i:02d}, ", end="")
 
   print()
+
 
 if __name__ == "__main__":
   print_numbers(0, 100)
