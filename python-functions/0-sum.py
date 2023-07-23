fdb31d9c6d0289e31177a9a049f9292d99dbceaf
sgkanya <sgkanyae@gmail.com>
2023-07-23 21:52:11 +0100

    Fuctions

---
+++ python-functions/0-sum.py
@@ -0,0 +1,13 @@
+def add(a, b):
+  """
+  Adds two integers and returns the result.
+
+  Args:
+    a: The first integer to add.
+    b: The second integer to add.
+
+  Returns:
+    The sum of a and b.
+  """
+
+  return a + b
