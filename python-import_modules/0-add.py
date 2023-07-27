8d5553f80d000c4252ef1ef0516ac97df1ddfe6a
sgkanya <sgkanyae@gmail.com>
2023-07-27 13:33:37 +0100

    module

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -3,4 +3,4 @@ from add_0 import add
 a = 1
 b = 2
 
-print(f"{a} + {b} = {add(a, b)}.format(a=a, b=b, add=add)\n0-add.py doesn't contain: [\"__main__\", \"__name__\"]")
+print(f"{a} + {b} = {(a, b)}.format(a=a, b=b, add=add)\n0-add.py doesn't contain: [\"__main__\", \"__name__\"]")
