e2190b54dfe990e9091382fea0c704616f9ca098
sgkanya <sgkanyae@gmail.com>
2023-07-27 22:31:42 +0100

    updated

--- python-import_modules/2-variable_load.py
+++ python-import_modules/2-variable_load.py
@@ -1,7 +1,7 @@
 def print_a():
     with open("variable_load_2.py", "r") as f:
         source = f.read()
-
+    variable = a
     # Extract the variable a from the source code
     exec(source)
     print(a)
