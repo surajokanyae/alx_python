c6194885e13a13e0756416511c7c4610812af459
sgkanya <sgkanyae@gmail.com>
2023-07-27 17:04:35 +0100

    arguments

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -3,12 +3,9 @@ import sys
 def print_arguments():
     """Prints the number of and the list of its arguments."""
     args_number = len(sys.argv)
-    print(f"{args_number} arguments:")
-    if args_number == 0:
-        print(".")
-    else:
-        for i in range(1, args_number + 1):
-            print(f"{i}: {sys.argv[i - 1]}")
+    print(f"{args_number - 1} arguments:")
+    for i in range(1, args_number):
+        print(f"{i}: {sys.argv[i - 1]}")
 
 if __name__ == "__main__":
     print_arguments()
