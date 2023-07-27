e582e4d6552c2b1e4dadb3e11c4bab8ed647ba8a
sgkanya <sgkanyae@gmail.com>
2023-07-27 17:17:47 +0100

    variable

--- python-import_modules/2-variable_load.py
+++ python-import_modules/2-variable_load.py
@@ -1,8 +1,9 @@
-import importlib
+import sys
 
 def import_variable(variable_name):
     """Imports the variable from the file variable_load_2.py and prints its value."""
-    module = importlib.import_module("variable_load_2")
+    module_name = "variable_load_2"
+    module = sys.modules[module_name]
     variable = getattr(module, variable_name)
     print(variable)
 
