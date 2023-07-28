a28cd91a316c9daa3df56ec6998aac1efc2aba63
sgkanya <sgkanyae@gmail.com>
2023-07-28 16:43:46 +0100

    matric

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -14,7 +14,7 @@ def print_matrix_integer(matrix=[[]]):
 
   for row in matrix:
     for item in row:
-      print("{}{}".format(".format(", item), end=" ")
+      print("{:3}".format(item), end=" ")
     print()
 
 
