e3923c10ab1a54a8d18a5f0f2587dd8ea417339a
sgkanya <sgkanyae@gmail.com>
2023-07-28 16:36:39 +0100

    matric

---
+++ python-data_structures/1-print_matrix_integer.py
@@ -0,0 +1,34 @@
+def print_matrix_integer(matrix=[[]]):
+  """Prints a matrix of integers.
+
+  Args:
+    matrix: The matrix to be printed.
+
+  Returns:
+    Nothing.
+  """
+
+  if not matrix:
+    print("")
+    return
+
+  for row in matrix:
+    for item in row:
+      print(f"{item:3}", end=" ")
+    print()
+
+
+def main():
+  matrix = [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9]
+  ]
+
+  print_matrix_integer(matrix)
+  print("--")
+  print_matrix_integer()
+
+
+if __name__ == "__main__":
+  main()
