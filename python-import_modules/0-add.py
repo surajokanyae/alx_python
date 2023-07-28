4051b5502d73451a9b08276ed6c41c70923747fc
sgkanya <sgkanyae@gmail.com>
2023-07-28 16:27:36 +0100

    updated

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -12,17 +12,9 @@ def add(a, b):
     """
     return (a + b)
 
-def fake_add(a, b):
-    """My fake addition function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a - b
-    """
-    return (a - b)
+    def add(a, b):
+   
+     return (a - b)
 
 if __name__ == "__main__":
     a = 1
