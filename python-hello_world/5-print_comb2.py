1f1d157a3717b2e8bf1a21f60acde29ee6d2a6d6
sgkanya <sgkanyae@gmail.com>
2023-07-29 15:13:09 +0100

    conc

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,14 +1,3 @@
-def saparate_numbers():
-  """Prints numbers from 0 to 99."""
-  for i in range(100):
-    # Format the number as a two-digit string.
-    number = "{:02d}".format(i)
-    # Print the number, followed by a comma and a space.
-    print(number, end=", ")
-  # Print a newline at the end of the output.
-  print()
-
-
-if __name__ == "__main__":
-  saparate_numbers()
-
+for i in range(0, 99):
+    print("{:02d}, ".format(i), end='')
+print("99")
