a89d89a94182b6ca91c6a533bd5c427e42391c88
sgkanya <sgkanyae@gmail.com>
2023-07-29 15:14:38 +0100

    conc

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,11 +1,4 @@
-def print_comb3():
-  """Prints all possible different combinations of two digits."""
-  for i in range(10):
-    for j in range(i + 1, 10):
-      if i != j:
-        print("{0:02d}, ".format(i * 10 + j), end="")
-
-  print()
-
-if __name__ == "__main__":
-  print_comb3()
+for i in range(0, 100):
+    if (i % 10) > (i // 10) and i != 89:
+        print("{:02d}, ".format(i), end='')
+print("89")
