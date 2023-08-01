a2cf204eac482630f254e013ac8999d7a016f816
sgkanya <sgkanyae@gmail.com>
2023-08-01 17:35:28 +0100

    map

---
+++ python-more_data_structures/4-multiply_list_map.py
@@ -0,0 +1,21 @@
+def multiply_list_map(my_list=[], number=0):
+  """Returns a list with all values multiplied by a number without using any loops.
+
+  Args:
+    my_list: The list to multiply.
+    number: The number to multiply by.
+
+  Returns:
+    A new list:
+      Same length as my_list
+      Each value should be multiplied by number
+    Initial list should not be modified
+  """
+
+  return list(map(lambda x: x * number, my_list))
+
+
+if __name__ == "__main__":
+  my_list = [1, 2, 3, 4, 5]
+  new_list = multiply_list_map(my_list, 2)
+  print(new_list)
