9fd172cebf8fb6515d8c81cd8c13ebb54559a946
sgkanya <sgkanyae@gmail.com>
2023-08-01 18:10:12 +0100

    Map

--- python-more_data_structures/4-multiply_list_map.py
+++ python-more_data_structures/4-multiply_list_map.py
@@ -1,3 +1,3 @@
-def square_matrix_map(matrix=[]):
-    x = [1, 2, 3, 4, 5]
+#!/usr/bin/python3
+def multiply_list_map(my_list=[], number=0):
     return(list(map(lambda row: list(map(lambda x: x*x, row)), matrix)))
