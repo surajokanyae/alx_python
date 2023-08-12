a60ce712bdda9e6a7f45e342be82d73225da6b73
sgkanya <sgkanyae@gmail.com>
2023-08-12 15:34:28 +0100

    Status

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,7 +1,25 @@
+"""
+This module fetches the status of the intranet website.
+
+The module exports the following functions:
+
+* main(): Gets the status of the intranet website and prints it to the console.
+
+The module also exports the following variables:
+
+* url: The URL of the intranet website.
+
+The module raises the following exceptions:
+
+* requests.exceptions.HTTPError: If the request to the intranet website fails.
+"""
+
 import requests
 
+url = 'https://alu-intranet.hbtn.io/status'
+
 def main():
-    url = 'https://alu-intranet.hbtn.io/status'
+    """Gets the status of the intranet website and prints it to the console."""
     response = requests.get(url)
 
     if response.status_code == 200:
