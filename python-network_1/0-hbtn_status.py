495114e151b0ca97727bb297114457e2f6de1afe
sgkanya <sgkanyae@gmail.com>
2023-08-12 16:14:45 +0100

    Status

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -13,22 +13,21 @@ The module raises the following exceptions:
 
 * requests.exceptions.HTTPError: If the request to the intranet website fails.
 """
-
 import requests
 
 def main():
-    url = 'https://alu-intranet.hbtn.io/status'
+    url = 'https://intranet.hbtn.io/status'
     response = requests.get(url)
 
     if response.status_code == 200:
         body = response.content.decode('utf-8')
         print('Body response:')
-        for key, value in body.items():
-            print(f'\t- type: {type(value)}')
-            print(f'\t- content: {value}')
+        print('\t- type: {}'.format(type(body)))
+        print('\t- content: {}'.format(body))
     else:
         print(f'Get failed: {response.status_code}')
 
 if __name__ == '__main__':
     main()
+
 
