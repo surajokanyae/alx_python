0860af1fafd049920e26709bda7cfde12836f72c
sgkanya <sgkanyae@gmail.com>
2023-08-12 15:43:14 +0100

    Status

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -19,8 +19,8 @@ import requests
 if __name__ == "__main__":
     import urllib.request as request
     with request.urlopen('https://intranet.hbtn.io/status') as response:
-        content = response.read()
+        content = "response.read()"
         print("Body response:")
-        print("\t- type: {}".format(type.string(content)))
+        print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
     
